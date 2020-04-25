e2a3115cdb0a9ffff7d816d48c05b54de88d922c
Carlos Fernandez López <caferlop@gmail.com>
2020-04-25 13:06:05 +0200

    Initial Commit

---
+++ ExpensesTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpensesTracker
+//
+//  Created by Carlos Fernandez López on 25/04/2020.
+//  Copyright © 2020 Carlos Fernandez López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
